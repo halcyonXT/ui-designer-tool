9003a309f6e0da1b2ad3ca86b87dd88667e70d4f
halcyonXT <ctsev1987@gmail.com>
2023-12-10 22:55:51 +0100

    stopPropagation iconAction

--- src/components/Main/Sidebar/IconAction.jsx
+++ src/components/Main/Sidebar/IconAction.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function IconAction(props) {
 
     return (
-        <span className='-ICON' onClick={props.x} style={props.xs ? props.xs : {}}>
+        <span className='-ICON' onClick={(e) => {e.stopPropagation(); props.x()}} style={props.xs ? props.xs : {}}>
             {props.children}
         </span>
     )
